5320e95920845cc20a38a7ab6fa400cd7cec84a7
Milton Paredes <mhplur@hotmail.com>
2022-06-15 22:01:57 -0500

    Update test

---
+++ microservicio/dominio/src/test/java/com/ceiba/cita/SolicitarCitaTestDataBuilder.java
@@ -0,0 +1,49 @@
+package com.ceiba.cita;
+
+import com.ceiba.cita.modelo.entidad.SolicitarCita;
+import com.ceiba.client.entidad.Client;
+import com.ceiba.tarifa.entidad.Tarifa;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class SolicitarCitaTestDataBuilder {
+
+    private Client cliente;
+    private Tarifa tarifa;
+    private LocalDate fechaCita;
+    private LocalTime horaCita;
+
+    private String horario;
+
+
+    public SolicitarCitaTestDataBuilder conClient(Client client) {
+        this.cliente = client;
+        return this;
+    }
+
+    public SolicitarCitaTestDataBuilder conTarifa(Tarifa tarifa) {
+        this.tarifa = tarifa;
+        return this;
+    }
+
+    public SolicitarCitaTestDataBuilder conFechaCita(LocalDate fechaCita) {
+        this.fechaCita = fechaCita;
+        return this;
+    }
+
+    public SolicitarCitaTestDataBuilder conHoraCita(LocalTime horaCita) {
+        this.horaCita = horaCita;
+        return this;
+    }
+
+    public SolicitarCitaTestDataBuilder conHorario(String horario) {
+        this.horario = horario;
+        return this;
+    }
+
+    public SolicitarCita build() {
+        return new SolicitarCita(cliente, tarifa, fechaCita, horaCita, horario);
+    }
+
+}
