873a338a248260d1b4668e8453ea318e88ce2121
Milton Paredes <mhplur@hotmail.com>
2022-06-29 17:31:47 -0500

    Update ServicioCitarTest.java

--- microservicio/dominio/src/test/java/com/ceiba/cita/ServicioCitarTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/cita/ServicioCitarTest.java
@@ -4,6 +4,7 @@ import com.ceiba.cita.modelo.entidad.Cita;
 import com.ceiba.cita.puerto.repositorio.RepositorioCita;
 import com.ceiba.cita.servicio.ServicioCitar;
 import com.ceiba.client.entidad.Client;
+import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 import com.ceiba.tarifa.entidad.Tarifa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -45,5 +46,22 @@ public class ServicioCitarTest {
         Assertions.assertEquals(2, captorCita.getValue().getMetrosCuadrados());
         Assertions.assertEquals(1, captorCita.getValue().getEstado());
         Assertions.assertEquals(1L, idCitaCreada);
+    }
+
+    @Test
+    void deberiaLanzarExcepcionPorParametroNulo() {
+        Client client = new Client(1l, "001", "MILTON PAREDES", "QUITO");
+        Tarifa tarifa = new Tarifa(1l, "VAPOR O AGUA CALIENTE", "W001", 24, 15, 1);
+        var solicitarCita = new SolicitarCitaTestDataBuilder()
+                .conClient(client)
+                .conTarifa(tarifa)
+                .conFechaCita(LocalDate.parse("2022-09-17"))
+                .conHoraCita(LocalTime.parse("05:00:00"))
+                .conHorario("DIA")
+                .conMetrosCuadrado(2)
+                .build();
+        var repositorioCita = Mockito.mock(RepositorioCita.class);
+        var servicioCitar = new ServicioCitar(repositorioCita);
+        Assertions.assertThrows(ExcepcionValorInvalido.class, () -> servicioCitar.ejecutar(solicitarCita));
     }
 }
